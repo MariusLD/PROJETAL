49d7569ae04e6d2a881f4dff9bad78fcd0d424cb
Yanis Bouger <yanis.bouger@gmail.com>
2023-01-12 22:06:09 +0100

    plus on avance moins ça marche

--- quarkus/src/main/java/org/acme/RmqConsumer.java
+++ quarkus/src/main/java/org/acme/RmqConsumer.java
@@ -1,29 +1,43 @@
 package org.acme;
 
 import java.nio.charset.StandardCharsets;
-
 import javax.enterprise.context.ApplicationScoped;
-
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
-import io.quarkus.logging.Log;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.reactive.ReactiveMailer;
+import io.smallrye.mutiny.Uni;
 import io.vertx.core.json.JsonObject;
+import com.oracle.svm.core.annotate.Inject;
 
 @ApplicationScoped
 public class RmqConsumer {
+
+     @Inject
+    ReactiveMailer mailer;
 
     @Incoming("mail")
     public void consume(byte[] msg) {
         JsonObject obj = new JsonObject(new String(msg, StandardCharsets.UTF_8));
         JsonObject data = new JsonObject(obj.getString("data"));
         
-        MailSender.send(
+        send(
             data.getString("from"),
             data.getString("to"),
             data.getString("subject"),
             data.getString("body")
         );
         
+    }
+
+    public Uni<Void> send(String from, String to, String subject, String body) {
+        return mailer.send(
+            Mail.withText(
+                to,
+                subject,
+                body
+            ).setFrom(from)
+        );
     }
     
 }
